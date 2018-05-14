2948810aa6af28c99ac0c1111082aef9d0316955
JohnOMalleyNewBury <john.omalley@newbury.edu>
2018-05-14 12:25:12 -0400

    Initial commit.

---
+++ TestBoard.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+__author__ = 'John Omalley'
+
+from gameObjects import Board
+
+testBoard = Board
+testBoard.setmark[0]
+testBoard.setMark[1]
+testBoard.setMark[2]
+testBoard.getMark(0, 1, 2)
+
