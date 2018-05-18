fef244a4613f419f6e8c165a60a07b28d158fefd
JohnOMalleyNewBury <john.omalley@newbury.edu>
2018-05-18 12:29:27 -0400

    Initial commit.

--- TestBoard.py
+++ TestBoard.py
@@ -4,12 +4,13 @@ __author__ = 'John Omalley'
 from gameObjects import Board
 
 testBoard = Board()
-testBoard.setMark(0, 'X')
-testBoard.setMark(1, 'X')
-testBoard.setMark(3, 'O')
-print(testBoard.getMark(0))
-print(testBoard.getMark(1))
-print(testBoard.getMark(2))
+testBoard.setMark(3, 'X')
+testBoard.setMark(4, 'X')
+testBoard.setMark(7, 'O')
 print(testBoard.getMark(3))
+print(testBoard.getMark(4))
+print(testBoard.getMark(7))
+
 
 print(testBoard.findBlock('O'))
+print(testBoard.findWin('X'))
